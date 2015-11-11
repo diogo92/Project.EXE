28f38c2fd3ba6e1c0477c800015f8ea769105b26
diogo92 <diogo_p_ribeiro@hotmail.com>
2015-11-11 18:16:03 +0000

    Initial commit

---
+++ Advanced Character Controller Unity 5/Assets/Scripts/Character Controller/CharMove.cs
@@ -0,0 +1,221 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMove : MonoBehaviour
+{
+
+    float moveSpeedMultiplier = 1;
+    float stationaryTurnSpeed = 180; 
+    float movingTurnSpeed = 360;
+
+    public bool onGround; 
+
+    Animator animator;
+
+    Vector3 moveInput; 
+    float turnAmount; 
+    float forwardAmount; 
+    Vector3 velocity;
+    float jumpPower = 10;
+   
+    IComparer rayHitComparer;
+    Rigidbody rigidBody;
+
+	float lastAirTime;
+	Collider col;
+	public PhysicMaterial highFriction;
+	public PhysicMaterial lowFriction;
+
+	float autoTurnThreshold = 10;
+	float autoTurnSpeed = 20;
+	bool aim;
+	Vector3 currentLookPos;
+   
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+
+        SetUpAnimator();
+
+        rigidBody = GetComponent<Rigidbody>();  
+
+		col = GetComponent<Collider> ();
+    }
+
+    void SetUpAnimator()
+    {
+        animator = GetComponent<Animator>();
+
+        foreach (var childAnimator in GetComponentsInChildren<Animator>())
+        {
+            if (childAnimator != animator)
+            {
+                animator.avatar = childAnimator.avatar;
+                Destroy(childAnimator);
+                break; 
+            }
+        }
+
+    }
+ 
+    void OnAnimatorMove()
+    {
+        if (onGround && Time.deltaTime > 0)
+        {
+            Vector3 v = (animator.deltaPosition * moveSpeedMultiplier) / Time.deltaTime; 
+			//Delta position (position difference) - The difference in position between last frame and current one
+			//Speed = position difference of the animator * speed multiplier / time
+            v.y = rigidBody.velocity.y; //store the characters vertical speed, so it does not affect jump speed
+            rigidBody.velocity = v; // update character's speed
+        }
+    }
+
+    public void Move(Vector3 move, bool aim, Vector3 lookPos)
+    {    
+		//Vector3 move is the input in the world space
+        if (move.magnitude > 1) //Make sure that the movement is normalized
+            move.Normalize();
+       
+        this.moveInput = move; //Store the move
+		this.aim = aim;
+		this.currentLookPos = lookPos;
+
+        velocity = rigidBody.velocity; //Store current velocity
+
+        ConvertMoveInput();
+
+		if (!aim) {
+			ApplyExtraTurnRotation ();
+			TurnTowardsCameraForward ();
+		}
+
+
+        ApplyExtraTurnRotation();
+        GroundCheck ();
+		SetFriction ();
+
+		if (onGround) {
+			HandleGroundVelocities ();
+		} else {
+
+			HandleAirborneVelocities();
+		}
+        UpdateAnimator();
+
+    }
+
+    void ConvertMoveInput()
+    {
+        Vector3 localMove = transform.InverseTransformDirection(moveInput);
+
+        turnAmount = Mathf.Atan2(localMove.x, localMove.z);
+
+        forwardAmount = localMove.z;
+    }
+
+    void ApplyExtraTurnRotation()
+    {
+        float turnSpeed = Mathf.Lerp(stationaryTurnSpeed, movingTurnSpeed, forwardAmount);
+        transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+    }
+
+    void UpdateAnimator()
+    { 
+		animator.applyRootMotion = true;
+
+		if (!aim) {
+			animator.SetFloat ("Forward", forwardAmount, 0.1f, Time.deltaTime);
+			animator.SetFloat ("Turn", turnAmount, 0.1f, Time.deltaTime);
+		}
+		animator.SetBool("Aim",aim);
+		animator.SetBool ("OnGround", onGround);
+		
+    }
+   
+    void GroundCheck()
+    {
+        Ray ray = new Ray(transform.position + Vector3.up * .1f, -Vector3.up);
+
+        RaycastHit[] hits = Physics.RaycastAll(ray, .1f);
+        rayHitComparer = new RayHitComparer();
+        System.Array.Sort(hits, rayHitComparer);
+
+        if (velocity.y < jumpPower * .5f)
+        { 	
+            onGround = false;
+            rigidBody.useGravity = true;
+
+            foreach (var hit in hits)
+            { 
+                if (!hit.collider.isTrigger)
+                {
+                    if (velocity.y <= 0)
+                    {
+                          rigidBody.position = Vector3.MoveTowards(rigidBody.position, hit.point, Time.deltaTime * 100);
+                    }
+
+                    onGround = true; 
+                    rigidBody.useGravity = false; 
+                }
+            }
+        }
+
+		if (!onGround) {
+			lastAirTime = Time.time;
+		}
+    }
+
+	void TurnTowardsCameraForward(){
+		if (Mathf.Abs (forwardAmount) < .01f) {
+			Vector3 lookDelta = transform.InverseTransformDirection(currentLookPos - transform.position);
+
+			float lookAngle = Mathf.Atan2(lookDelta.x,lookDelta.z) * Mathf.Rad2Deg;
+
+			if(Mathf.Abs(lookAngle) > autoTurnThreshold){
+				turnAmount += lookAngle * autoTurnSpeed * .001f;
+			}
+		}
+	}
+
+
+	void SetFriction(){
+		if (onGround) {
+			if (moveInput.magnitude == 0) {
+				col.material = highFriction;
+			} else {
+				col.material = lowFriction;
+			}
+		} else {
+			col.material = lowFriction;
+		}
+	}
+
+	void HandleGroundVelocities(){
+		velocity.y = 0;
+
+		if (moveInput.magnitude == 0) {
+			velocity.x = 0;
+			velocity.z = 0;
+		}
+	}
+
+	void HandleAirborneVelocities(){
+		Vector3 airMove = new Vector3 (moveInput.x * 6, velocity.y, moveInput.z * 6);
+		velocity = Vector3.Lerp (velocity, airMove, Time.deltaTime * 2);
+
+		rigidBody.useGravity = true;
+
+		Vector3 extraGravityForce = (Physics.gravity * 2);
+
+		rigidBody.AddForce (extraGravityForce);
+	}
+
+    class RayHitComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((RaycastHit)x).distance.CompareTo(((RaycastHit)y).distance);
+        }
+    }
+
+}
---
+++ Advanced Character Controller Unity 5/Assets/Scripts/Character Controller/UserInput.cs
@@ -0,0 +1,208 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UserInput : MonoBehaviour
+{
+
+    public bool walkByDefault = false;
+
+    private CharMove character;
+    private Transform cam;
+    private Vector3 camForward;
+    private Vector3 move;
+
+	//Camera
+	//float cameraForward;
+	public bool aim;
+	public float aimingWeight;
+
+	public bool lookInCameraDirection;
+	Vector3 lookPos;
+
+	WeaponManager weaponManager;
+
+	public bool debugShoot;
+
+	WeaponManager.WeaponType weaponType;
+
+	CapsuleCollider col;
+	float startHeight;
+	Animator anim;
+	//Ik stuff
+
+	[SerializeField] public IK ik;
+	[System.Serializable] public class IK{
+		public Transform spine;
+		public float aimingZ = 213.46f;
+		public float aimingX = -65.97f;
+		public float aimingY = 20;
+		public float point = 30;
+		public bool DebugAim;
+	}
+
+
+
+    void Start()
+    {
+        if (Camera.main != null)
+        {
+            cam = Camera.main.transform;
+        }
+
+        character = GetComponent<CharMove>();
+
+		anim = GetComponent<Animator> ();
+
+		weaponManager = GetComponent<WeaponManager> ();
+    }
+
+	void CorrectIK(){
+		weaponType = weaponManager.weaponType;
+
+		if (!ik.DebugAim) {
+			switch(weaponType){
+			case WeaponManager.WeaponType.Pistol:
+				ik.aimingZ = 212.19f;
+				ik.aimingX = -63.8f;
+				ik.aimingY = 16.65f;
+				break;
+			case WeaponManager.WeaponType.Rifle:
+				ik.aimingZ = 212.19f;
+				ik.aimingX = -63.1f;
+				ik.aimingY = 14.1f;
+				break;
+
+			}
+		}
+	}
+
+	void Update(){
+
+		CorrectIK ();
+		if(!ik.DebugAim)
+		aim = Input.GetMouseButton (1);
+
+		weaponManager.aim = aim;
+		if (aim) {
+			if(!weaponManager.ActiveWeapon.CanBurst){
+				if(Input.GetMouseButtonDown(0) || debugShoot){
+					anim.SetTrigger("Fire");
+					weaponManager.FireActiveWeapon();
+				}
+			}else{
+				if(Input.GetMouseButton(0) || debugShoot){
+					anim.SetTrigger("Fire");
+					weaponManager.FireActiveWeapon();
+				}
+			}
+		}
+		if(Input.GetAxis("Mouse ScrollWheel") !=0 ){
+			if(Input.GetAxis("Mouse ScrollWheel") <= -0.1f){
+				weaponManager.ChangeWeapon(false);
+			}
+			
+			if(Input.GetAxis("Mouse ScrollWheel") > -0.1f){
+				weaponManager.ChangeWeapon(true);
+			}
+		}
+	}
+
+
+	void LateUpdate(){
+
+
+		aimingWeight = Mathf.MoveTowards (aimingWeight, (aim) ? 1.0f : 0.0f, Time.deltaTime * 5);
+
+		Vector3 normalState = new Vector3 (0, 0, -2f);
+		Vector3 aimingState = new Vector3 (0, 0, -0.5f);
+
+		Vector3 pos = Vector3.Lerp (normalState, aimingState, aimingWeight);
+
+		cam.transform.localPosition = pos;
+
+		if (aim) {
+			Vector3 eulerAngleOffset = Vector3.zero;
+			eulerAngleOffset = new Vector3(ik.aimingX,ik.aimingY,ik.aimingZ);
+
+			Ray ray = new Ray(cam.position, cam.forward);
+
+			Vector3 lookPosition = ray.GetPoint(ik.point);
+
+			ik.spine.LookAt(lookPosition);
+			ik.spine.Rotate(eulerAngleOffset,Space.Self);
+		}
+	}
+
+    void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+		if (!aim) {
+			if (cam != null) {
+				//Take the forward vector of the camera (from its transform) and
+				//eliminate the y component
+				//scale the camera forward with the mask (1,0,1) to eliminate y and normalize it
+				camForward = Vector3.Scale (cam.forward, new Vector3 (1, 0, 1)).normalized;
+	            
+				//move input front/backward = forward direction of the camera * user input amount (vertical)
+				//move input left/right = right direction of the camera * user input amount (horizontal)
+				move = vertical * camForward + horizontal * cam.right;
+			} else {
+
+				//if there is not a camera, use the global forward (+z) and right ( +x)
+				move = vertical * Vector3.forward + horizontal * Vector3.right;
+			}
+		} else {
+
+			move = Vector3.zero;
+
+			Vector3 dir = lookPos - transform.position;
+
+			dir.y = 0;
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+
+			anim.SetFloat("Forward",vertical);
+			anim.SetFloat("Turn", horizontal);
+
+		}
+        if (move.magnitude > 1) // make sure the movement is normalizd
+            move.Normalize();
+
+        bool walkToggle = Input.GetKey(KeyCode.LeftShift) || aim;
+
+        float walkMultiplier = 1;
+
+        if (walkByDefault)
+        {
+            if (walkToggle)
+            {
+                walkMultiplier = 1;
+            }
+            else
+            {
+                walkMultiplier = 0.5f;
+            }
+        }
+        else
+        {
+            if (walkToggle)
+            {
+                walkMultiplier = 0.5f;
+            }
+            else
+            {
+                walkMultiplier = 1;
+            }
+        }
+
+		lookPos = lookInCameraDirection && cam != null ? transform.position + cam.forward * 100 : transform.position + transform.forward * 100;
+
+        move *= walkMultiplier;
+        character.Move(move,aim,lookPos);
+    }
+
+}
